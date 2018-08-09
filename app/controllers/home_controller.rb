22626878d89bddfae4ebaee279bb388ca71f96c2
Yukito Ito <yukibukiyou@gmail.com>
2018-08-09 19:27:13 +0900

    refactoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
     @songs = Song.order_by_count.take(15)
-    logger.debug request.ip
   end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,17 @@
 class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
   def show
     @song = Song.find(params[:id])
+  end
+
+  def quick_votes
+    @song = Song.find(params[:song_id])
+    vote = @song.votes.build(ip: request.remote_ip)
+    vote.save!
+    redirect_to song_path(@song), notice: "#{@song.title}に投票したよ"
   end
 
   def just_created?
